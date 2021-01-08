0308f9c9a2c230e59a422cc8621b4e1d2b055c30
aretiz <zervou@csd.uoc.gr>
2021-01-08 17:39:34 +0200

    Add files via upload

--- source_code/HVG_and_RQA.m
+++ source_code/HVG_and_RQA.m
@@ -86,12 +86,18 @@ clear feat
 %% HVG & MF-RQA
 load('DATA.mat')
 [emb1,emb2,tau1,tau2] = find_mode_m_t_RQA(data);
-perc = 0.3;
+perc = 0.2;
 MF_RQA
 load('hvg_feat.mat')
 feat = [feat hvg_feat];
 FDA_classifier
 fprintf('The classification accuracy of MF-RQA and HVG for FDA is %f.\n',mean(acc));
+clear feat
+
+perc = 0.3;
+MF_RQA
+load('hvg_feat.mat')
+feat = [feat hvg_feat];
 SVM_classifier
 fprintf('The classification accuracy of MF-RQA and HVG for SVM is %f.\n',mean(acc));
 
@@ -101,11 +107,17 @@ clear feat
 load('DATA.mat')
 [emb1,tau1] = find_mode_m_t_GmdRQA(data);
 
-perc = 0.3;
-MF_RQA
+perc = 0.2;
+MF_GmdRQA
 load('hvg_feat.mat')
 feat = [feat hvg_feat];
 FDA_classifier
 fprintf('The classification accuracy of MF-GmdRQA and HVG for FDA is %f.\n',mean(acc));
+clear feat
+
+perc = 0.3;
+MF_GmdRQA
+load('hvg_feat.mat')
+feat = [feat hvg_feat];
 SVM_classifier
 fprintf('The classification accuracy of MF-GmdRQA and HVG for SVM is %f.\n',mean(acc));
